f01b1f01047c554c05d57e5d1be9fb94b92885fa
Caleb Chiang <calebchiang1002@gmail.com>
2024-11-08 17:00:14 -0800

    added home page and introduction page routes

---
+++ _build/default/src/handlers.ml
@@ -0,0 +1,10 @@
+(** Function for the homepage content *)
+let home_page _ =
+  Dream.html "<h1> Welcome to the Dream Framework</h1>
+              <p> Dream is a simple, type-safe web framework for OCaml. </p>"
+
+
+(** Function for the introduction page content *)
+let introduction_page _ = 
+  Dream.html "<h1> Introduction </h1> 
+              <p> Here is an introduction to the Dream framework. </p>"
--- src/main.ml
+++ src/main.ml
@@ -1,9 +1,11 @@
+open Handlers
+
+(** Entry point for web server *)
 let () =
   Dream.run
   @@ Dream.logger
   @@ Dream.router [
-    Dream.get "/" (fun _ ->
-      Dream.html "<h1>Welcome to the Dream Framework</h1>
-                  <p>Dream is a simple, type-safe web framework for OCaml.</p>");
+    Dream.get "/" home_page;
+    Dream.get "/introduction" introduction_page;
     Dream.any "/" (fun _ -> Dream.empty `Not_Found)
   ]
